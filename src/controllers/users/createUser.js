0b6455c5456ae7ffcec6ede3eeb2be2d574a1878
jonatassborges <jonatasbnino@gmail.com>
2023-10-26 15:31:42 -0300

    password encrypted

--- src/controllers/users/createUser.js
+++ src/controllers/users/createUser.js
@@ -1,29 +1,33 @@
-import user from '../../models/userModel.js'
-const createUser = async (req, res, next) => {
+import user from "../../models/userModel.js";
+import bcrypt from "bcrypt";
+
+const createUser = async (req, res) => {
     try {
-        const userData= req.body
-        const [result] = await user.insert(userData)
-
+      const userData = req.body;
+      bcrypt.hash(userData.pass, 10, async (error, hash) => {
+        if (error)
+          return res.status(500).json({
+            error: "Erro ao gerar senha criptografada!",
+          });
+        userData.pass = hash;
+        const [result] = await user.insert(userData);
+        delete userData.pass;
         if (result.affectedRows === 1) {
-            res.json({
-                success: "user succesfully created",
-                user: {
-                    id: result.insertId,
-                    ...userData
-                }
-            })
-        } else{
-            res.status(404).json({err: `fill the data`}) 
+          res.json({
+            success: "Usuário inserido com Sucesso!",
+            user: {
+              id: result.insertId,
+              ...userData,
+            },
+          });
         }
-
-
-
+      });
     } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            error: "Server Error"
-        })
-}
-
-}
-export default createUser
+      console.log(error);
+      res.status(500).json({
+        error: "Erro no servidor!",
+      });
+    }
+  };
+  
+  export default createUser;
