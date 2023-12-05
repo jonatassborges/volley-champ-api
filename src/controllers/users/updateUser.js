0a959182bd7acba1e7e78e7ff5a5039553f85fb8
jonatassborges <jonatasbnino@gmail.com>
2023-12-05 14:57:01 -0300

    pass encrip at update

--- src/controllers/users/updateUser.js
+++ src/controllers/users/updateUser.js
@@ -1,4 +1,5 @@
 import user from "../../models/userModel.js";
+import bcrypt from "bcrypt";
 
 const putUser = async (req, res, next) => {
   try {
@@ -8,17 +9,27 @@ const putUser = async (req, res, next) => {
         error: `Usuário não autorizado a fazer update.`,
       });
     }
-    const [result] = await user.update(userData);
-    if (result.affectedRows === 1) {
-      res.json({
-        success: "Usuário atualizado com Sucesso!",
-        user: {
-          ...userData,
-        },
-      });
-    } else {
-      res.status(404).json({
-        error: `Usuário id: ${userData.id} não Encontrado!`,
+    if (userData.pass) {
+      bcrypt.hash(userData.pass, 10, async (error, hash) => {
+        if (error)
+          return res.status(500).json({
+            error: "Erro ao gerar senha criptografada!",
+          });
+        userData.pass = hash;
+        const [result] = await user.update(userData);
+        delete userData.pass;
+        if (result.affectedRows === 1) {
+          res.json({
+            success: "Usuário atualizado com Sucesso!",
+            user: {
+              ...userData,
+            },
+          });
+        } else {
+          res.status(404).json({
+            error: `Usuário id: ${userData.id} não Encontrado!`,
+          });
+        }
       });
     }
   } catch (error) {
